fc0329a1053bd7c951de5ee40d2f3a64f91b65f2
MrFox-L <feralseattle@gmail.com>
2017-09-07 16:58:37 -0400

    Let's see.

--- App.js
+++ App.js
@@ -1,11 +1,19 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { 
+  StyleSheet, 
+  Text, 
+  View,
+  TouchableHighlight,
+   } from 'react-native';
 
 export default class App extends React.Component {
   render() {
     return (
       <View style={styles.container}>
         <Text>Open up App.js to start working on your app!</Text>
+        <TouchableHighlight>
+          <Text>Touch Me!</Text>
+        </TouchableHighlight>
       </View>
     );
   }
