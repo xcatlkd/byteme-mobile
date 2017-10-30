9928c5fcc712533cc1288192c2c7f988dd69e05a
Mr Fox-L <feralseattle#gmail.com>
2017-10-30 14:21:09 -0400

    Redux navtigation. Currently rendering. Need to add routing.

---
+++ components/LoginScreen.js
@@ -0,0 +1,115 @@
+import React from 'react';
+import { connect } from 'react-redux';
+import {
+				StyleSheet,
+				TouchableOpacity,
+				Text,
+			 	Image,
+			 	KeyboardAvoidingView }  from 'react-native';
+
+import { Button,
+				 Container,
+				 Content,
+				 Form,
+				 Item,
+				 Input,
+				 Toast } from "native-base";
+
+import { FormLabel,
+				 FormInput,
+				 FormValidationMessage } from 'react-native-elements';
+
+import { signup } from '../actions/users';
+
+class SignUp extends React.Component {
+	constructor(props) {
+		super(props); {
+			this.state = {
+				username: "",
+				password: "",
+			};
+		}
+	}
+
+	_handleSubmit = (event) => {
+		event.preventDefault();
+		console.log("handleSubmit; signup:   this.props: ", this.props)
+		this.props.signup(this.state);
+
+	}
+
+
+	render() {
+		return(
+			<Container style={styles.container}>
+				<KeyboardAvoidingView style={styles.container} behavior="padding">
+					<FormLabel style={styles.form}>
+						Username:
+					</FormLabel>
+					<FormInput style={styles.input} onChangeText={(username) => this.setState({username})}
+			    value={this.state.username}/>
+					<FormValidationMessage style={styles.error}>
+						{'This field is required'}
+					</FormValidationMessage>
+					<FormLabel>
+						Password:
+					</FormLabel>
+					<FormInput style={styles.input} onChangeText={(password) => this.setState({password})}
+					value={this.state.password}/>
+					<FormValidationMessage style={styles.error}>
+						{'This field is required'}
+					</FormValidationMessage>
+					<Button style={styles.button} onPress={this._handleSubmit}><Text>Sign Up</Text></Button>
+				</KeyboardAvoidingView>
+			</Container>
+
+		// <Image
+		// 	style={styles.logo}
+		// 	source={{uri: 'https://imgur.com/NL5irJA'}}
+		// />
+		)
+	}
+};
+
+function mapStateToProps(state, props) {
+	return {
+		isLoggedIn: state.users.isLoggedIn,
+		error: state.users.error,
+	};
+}
+
+export default connect(mapStateToProps, { signup })(SignUp);
+
+const styles = StyleSheet.create({
+	container: {
+		// flex: 1,
+		backgroundColor: '#F0F8FF',
+		alignItems: 'center',
+		justifyContent: 'center',
+		// padding: 20
+	},
+
+	form: {
+		justifyContent: 'center',
+	},
+
+	input: {
+		width: 225,
+		height: 40,
+	},
+
+	button: {
+		marginLeft: 150,
+		paddingBottom: 40,
+	},
+
+	error: {
+		marginBottom: 10,
+	},
+
+	logo: {
+		width: 66,
+		height: 58,
+	},
+
+});
---
+++ components/ProfileScreen.js
@@ -0,0 +1,78 @@
+import React from 'react';
+import {Image, View, Text, TouchableHighlight, StyleSheet}  from 'react-native';
+import {
+	Button,
+	Container,
+} from 'native-base';
+import { connect } from 'react-redux';
+import { getPhotos } from '../actions/images';
+
+class Home extends React.Component {
+	
+	_handleSubmit(event) {
+
+	}
+
+	componentDidMount() {
+		this.props.getPhotos();
+	}
+
+	render() {
+	let AuthButton;
+	let AuthButtonText;
+
+	if (!this.props.isLoggedIn) {
+		AuthButton = "Signup";
+		AuthButtonText = "Login or Signup!";
+	}
+	if (this.props.isLoggedIn) {
+		AuthButton = "UserPage";
+		AuthButtonText = "Check out dishes close to you now!";
+	}
+
+		return(
+			<Container style={styles.container} >
+				<View style={styles.container}>
+					<Text style={styles.titleText}>Welcome to Byte Me! A Food App to find tasty dishes
+					in your area! Let's get started.....</Text>
+					<Container style={styles.container} >
+						<Button style={styles.authButton} onPress={this._handleSubmit}><Text>{AuthButtonText}</Text></Button>
+					</Container >
+				</View>
+			</Container >
+		)
+	}
+
+
+};
+
+function mapStateToProps(state, props) {
+	return {
+		isLoggedIn: state.users.isLoggedIn,
+		// imagesLoaded: state.application.imagesLoaded,
+	};
+}
+
+export default connect(mapStateToProps, { getPhotos })(Home);
+
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#F0F8FF',
+    justifyContent: 'center',
+    alignItems: 'center',
+    paddingLeft: 25,
+    paddingRight: 25,
+  },
+  titleText: {
+  	flex: 2,
+  	paddingTop: 250,
+  	textAlign: 'center',
+  },
+  authButton: {
+  	flex: 1,
+  	justifyContent: 'center',
+  },
+
+});
